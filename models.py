c4b52da63a4db454ac0f70d05114b8144d53ba39
Adam Naim <adamahnaimah@gmail.com>
2025-04-23 02:39:12 +0800

    models and app code to test adding users

---
+++ models.py
@@ -0,0 +1,17 @@
+from database import create_connection
+
+def add_user (username, email, password):
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
+                   (username, email, password))
+    conn.commit()
+    conn.close()
+
+def get_users():
+    conn = create_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users")
+    users = cursor.fetchall()
+    conn.close()
+    return users
