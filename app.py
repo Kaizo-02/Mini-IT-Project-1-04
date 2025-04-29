b062e6a75d53a864af5e43f2f5e03f6ad8ac3bcd
Adam Naim <adamahnaimah@gmail.com>
2025-04-29 15:28:09 +0800

    added more functions and modified databse code

--- app.py
+++
@@ -1,8 +0,0 @@
-from database import create_tables
-from models import add_user, get_users
-
-add_user("zackone", "ikanbusuk@air.com", "ayam123")
-
-users = get_users()
-for user in users:
-    print(user)
